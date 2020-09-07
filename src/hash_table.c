a958a22dbd680add017d355b9a741f1a12e5e4d3
Chocho31 <chudomir.miroslavov.chenkov@gmail.com>
2020-09-07 19:25:07 +0300

    Added hash function

--- src/hash_table.c
+++ src/hash_table.c
@@ -4,10 +4,22 @@
 
 #include "hash_table.h"
 
+static int ht_hash(const char *s, const int a, const int mod) {
+    long hash = 0;
+    const int len_s = strlen(s);
+
+    for (int i = 0; i < len_s; i++) {
+        hash += (long) pow(a, len_s - (i + 1)) * s[i];
+        hash = hash % mod;
+    }
+
+    return (int) hash;
+}
+
 static ht_item* ht_new_item(const char *key, const char *value) {
     ht_item* item = malloc(sizeof(ht_item));
 
-    item->key = strdup(key)
+    item->key = strdup(key);
     item->value = strdup(value);
 
     return item;
@@ -19,7 +31,7 @@ static void ht_del_item(ht_item* item) {
     free(item);
 }
 
-ht_hash_table ht_new_hash_table() {
+ht_hash_table* ht_new_hash_table() {
     ht_hash_table* ht = malloc(sizeof(ht_hash_table));
 
     ht->size = 53;
--- src/hash_table.h
+++ src/hash_table.h
@@ -12,4 +12,11 @@ typedef struct {
     ht_item** items;
 } ht_hash_table;
 
+static int ht_hash(const char* s, const int a, const int m)
+static ht_item* ht_new_item(const char *key, const char *value);
+static void ht_del_item(ht_item* item);
+ht_hash_table* ht_new_hash_table();
+void ht_del_hash_table(ht_hash_table* ht);
+
+
 #endif

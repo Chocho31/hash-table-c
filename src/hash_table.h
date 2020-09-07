10a572fae51c116bb0dc7a5b019f90bff563f666
Chocho31 <chudomir.miroslavov.chenkov@gmail.com>
2020-09-07 21:16:18 +0300

    Removed static function declarations from header file

--- src/hash_table.h
+++ src/hash_table.h
@@ -13,17 +13,9 @@ typedef struct {
     ht_item** items;
 } ht_hash_table;
 
-static int ht_generic_hash(const char* s, const int a, const int m);
-static int ht_hash(const char* s, const int num_buckets, const int attempt);
-
-static ht_item* ht_new_item(const char *key, const char *value);
-static void ht_del_item(ht_item* item);
-
-static ht_hash_table* ht_new_sized_hash_table(const int size_index);
 ht_hash_table* ht_new_hash_table();
 void ht_del_hash_table(ht_hash_table* ht);
 
-static void ht_resize(ht_hash_table* ht, const int direction);
 
 void ht_insert(ht_hash_table* ht, const char* key, const char* value);
 char* ht_search(ht_hash_table* ht, const char* key);

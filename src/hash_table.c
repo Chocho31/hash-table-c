7f73f85d06096d25a4cab7a3b209378a36a6ce6e
Chocho31 <chudomir.miroslavov.chenkov@gmail.com>
2020-09-07 19:10:59 +0300

    Added implementations of functions for creating and deleting hash table items and hash tables

---
+++ src/hash_table.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "hash_table.h"
+
+static ht_item* ht_new_item(const char *key, const char *value) {
+    ht_item* item = malloc(sizeof(ht_item));
+
+    item->key = strdup(key)
+    item->value = strdup(value);
+
+    return item;
+}
+
+static void ht_del_item(ht_item* item) {
+    free(item->key);
+    free(item->value);
+    free(item);
+}
+
+ht_hash_table ht_new_hash_table() {
+    ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+
+    ht->size = 53;
+    ht->count = 0;
+    ht->items = calloc((size_t) ht->size, sizeof(ht_item*));
+
+    return ht;
+}
+
+void ht_del_hash_table(ht_hash_table* ht) {
+    for (int i = 0; i < ht->size; i++) {
+        ht_item* item = ht->items[i];
+
+        if (item != NULL) {
+            ht_del_item(item);
+        }
+    }
+
+    free(ht->items);
+    free(ht);
+}
--- src/hash_table.h
+++ src/hash_table.h
@@ -2,14 +2,14 @@
 #define HASH_TABLE_HEADER
 
 typedef struct {
-    char *key;
-    char *value;
+    char* key;
+    char* value;
 } ht_item;
 
 typedef struct {
     int size;
     int count;
-    ht_item **items;
+    ht_item** items;
 } ht_hash_table;
 
 #endif
